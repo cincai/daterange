0431147447ea0a4c732910efc144623380b06564
wilson <wilsonch@gmail.com>
2013-08-12 15:15:58 +1000

    Added clojure implementation

---
+++ src/main/clojure/daterange.clj
@@ -0,0 +1,40 @@
+(ns daterange)
+
+(defn is-leap [y] 
+  (if (= (mod y 4) 0)
+    (if (or (> (mod y 100) 0) (= (mod y 400) 0))
+      true false) false))
+
+(defn days-in-month [y m]
+  (let [thirties #{4 6 9 11}]
+    (if (= 2 m) 
+      (if (is-leap y) 29 28)
+      (if (contains? thirties m) 30 31))))
+
+(defn leap-years-past [y]
+  (let [years-past (- y 1)]
+    (+ (quot years-past 4) (quot years-past -100) (quot years-past 400))))
+
+(defn days-to-start-of-year [y]
+  (+ (* (- y 1) 365) (leap-years-past y)))
+
+(defn days-from-start-of-year [y m d]
+  (reduce + d (map #(days-in-month y %) (range 1 m))))
+
+(defn rank [y m d]
+  (+ (days-to-start-of-year y) (days-from-start-of-year y m d)))
+
+(defn days-between [start end]
+  (let [[y1 m1 d1] start
+        [y2 m2 d2] end]
+    (- (rank y2 m2 d2) (rank y1 m1 d1))))
+     
+(defn rank-by-month [y m d]
+  (let [years-past (- y 1)
+        full-months-past (+ (* 12 years-past) m -1)]
+    (+ full-months-past (/ d (days-in-month y m)))))
+
+(defn months-between [start end]
+  (let [[y1 m1 d1] start
+        [y2 m2 d2] end]
+    (- (rank-by-month y2 m2 d2) (rank-by-month y1 m1 d1))))
